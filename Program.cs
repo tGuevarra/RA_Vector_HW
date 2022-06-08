891c3ac03d8f6b991a13b3326c0f508d08744612
tGuevarra <104074409+tGuevarra@users.noreply.github.com>
2022-06-09 00:37:05 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace RA_Vector_HW
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Vector vector1 = new Vector(5, 6, 7);
+            Vector vector2 = new Vector(-3, 11, 9);
+
+            Console.WriteLine($"Вектор 1: {vector1.ToString()} \nВектор 2: {vector2.ToString()}");
+            Console.ReadLine();
+
+            Console.WriteLine($"Длины вектора 1: {vector1.squareRoot()} \nДлина вектора 2: {vector2.squareRoot()}");
+            Console.ReadLine();
+
+            Console.WriteLine($"Скалярное произведение вектора: {Vector.scalarMultiplication(vector1, vector2)}");
+            Console.ReadLine();
+
+            Console.WriteLine($"Векторное произведение с другим вектором: {(vector1 * vector2)}");;
+            Console.ReadLine();
+
+            Console.WriteLine($"Косинус угла между векторами: {Vector.cosAngle(vector1, vector2)}");
+            Console.ReadLine();
+
+            Console.WriteLine($"Сумма {(vector1 + vector2)} векторов \nРазность {(vector1 - vector2)} векторов");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Vector.cs
@@ -0,0 +1,41 @@
+﻿namespace RA_Vector_HW
+{
+    public class Vector
+    {
+        public int x, y, z;
+        public Vector(int xc, int yc, int zc)
+        {
+            x = xc;
+            y = yc;
+            z = zc;
+        }
+        public override string ToString()
+        {
+            return ($"x - {x}; y - {y}; z - {z}");
+        }
+        public double squareRoot()
+        {
+            return Math.Sqrt(x * x + y * y + z * z);
+        }
+        public static double scalarMultiplication(Vector k1, Vector k2)
+        {
+            return(k1.x * k2.x + k1.y * k2.y + k1.z * k2.z);
+        }
+        public static Vector operator * (Vector k1, Vector k2)
+        {
+            return new Vector(k1.y * k2.z - k1.z * k2.y, k1.z * k2.x - k1.x * k2.z, k1.x * k2.y - k1.y * k2.x);
+        }
+        public static double cosAngle(Vector k1, Vector k2)
+        {
+            return Vector.scalarMultiplication(k1, k2)/(k1.squareRoot()) *(k2.squareRoot());
+        }
+        public static Vector operator + (Vector k1, Vector k2)
+        {
+            return new Vector(k1.x + k2.x, k1.y + k2.y, k1.z + k2.z);
+        }
+        public static Vector operator - (Vector k1, Vector k2)
+        {
+            return new Vector(k1.x - k2.x, k1.y - k2.y, k1.z - k2.z);
+        }
+    }
+}
